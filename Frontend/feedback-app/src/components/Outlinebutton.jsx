4d393e3090ad92937f5c76aaaa86447727af2fd8
FrekeNobles <noblendifreke@gmail.com>
2025-08-13 17:24:59 +0100

    fix: merge fixs

--- Frontend/feedback-app/src/components/Outlinebutton.jsx
+++ Frontend/feedback-app/src/components/Outlinebutton.jsx
@@ -17,19 +17,3 @@ function Outlinebutton(props) {
 }
 
 export default Outlinebutton;
-
-
-// function Outlinebutton(props) {
-//   return (
-//     <Link to={`/${props.to}`}
-//         className='border h-[60px] border-[#3B027D] py-2.5 px-5 rounded-xl hover:cursor-pointer hover:bg-[#3B027D] text-[#3B027D] hover:text-white '>
-//         <div className='flex justify-between font-semibold text-xl '>
-//             <span>{props.text}</span>
-//             <span>&gt;</span>
-//         </div>
-       
-//     </Link>
-//   )
-// }
-
-// export default Outlinebutton
